31d1154461248a5f280ec0446156c8cafa7b991d
boonaki <joshdurr17@gmail.com>
2023-04-06 11:30:45 -0400

    added favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,13 @@
-import '@/styles/globals.css'
-import Layout from '../components/layout'
+import '@/styles/globals.css';
+import Layout from '../components/layout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
         <>
+            <Head>
+                <link rel="shortcut icon" href="/favicon.ico" />
+            </Head>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
