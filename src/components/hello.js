f7f6fd6075b6d8565eef745e742e8c991bbf985e
boonaki <joshdurr17@gmail.com>
2023-09-13 21:46:38 -0400

    added portugese 'oi'

--- src/components/hello.js
+++ src/components/hello.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 const Hello = () => {
     const [hello, setHello] = useState('Hello')
     const [idx, setIdx] = useState(0)
-    const hellos = ['¡Hola!', 'Bonjour!', 'Ciao!', 'Guten-tag!', 'Yokwe!', '你好!', 'こんにちは!', '안녕하세요!', 'Ya’at’eeh!', 'Hello!']
+    const hellos = ['¡Hola!', 'Bonjour!', 'Ciao!', 'Guten-tag!', 'Oi!', 'Yokwe!', '你好!', 'こんにちは!', '안녕하세요!', 'Ya’at’eeh!', 'Hello!']
     
     setInterval(() => {
         setIdx(idx + 1)
