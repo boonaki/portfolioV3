adeb03f0c62443f5f74fa2a25162b65bf4a77b73
boonaki <joshdurr17@gmail.com>
2025-04-06 08:43:07 -0400

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
         <>
             <Layout>
                 <Component {...pageProps} />
-                <Script defer src="https://quiet-biscotti-119867.netlify.app/script.js" data-website-id="efe0f749-bf18-48c1-b5d2-054382cf5584"></Script>
+                <Script defer src="https://analytics.lunarhat.com/script.js" data-website-id="efe0f749-bf18-48c1-b5d2-054382cf5584"></Script>
             </Layout>
         </>
     )
