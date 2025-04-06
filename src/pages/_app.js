45f56b88e163bf1a82fe0f24c6155ab17cbf29ec
boonaki <joshdurr17@gmail.com>
2025-04-06 08:40:04 -0400

    analytics update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
         <>
             <Layout>
                 <Component {...pageProps} />
-                <Script defer src="https://beamish-pastelito-7a7f06.netlify.app/script.js" data-website-id="d0eb31c4-0529-4d73-8ff7-cbfff9d80ed1"></Script>
+                <Script defer src="https://quiet-biscotti-119867.netlify.app/script.js" data-website-id="efe0f749-bf18-48c1-b5d2-054382cf5584"></Script>
             </Layout>
         </>
     )
